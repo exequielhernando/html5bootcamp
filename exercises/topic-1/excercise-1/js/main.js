78ac87a683464f92fbfa6e197a7fe88e252e94eb
Exequiel Hernando <exequielhernando@gmail.com>
2019-05-20 23:30:05 -0300

    Excercice 4 70%

--- exercises/topic-1/excercise-1/js/main.js
+++ exercises/topic-1/excercise-1/js/main.js
@@ -5,14 +5,20 @@ let nodo_section = document.getElementById("hello-world");
 let btn_get_response = document.querySelector("#greet");
 let hidden = setTimeout('hidden_section()', 3000);
 let api_response = document.getElementById("api-response");
+let btn_get_data_with_params = document.querySelector(".search-repository");
 const URL = "http://api.icndb.com/jokes/random";
+const URL2 = "https://api.github.com/search/repositories";
 console.log("Paso 3");
+
+btn_get_data_with_params.addEventListener("click", function(){
+    get_data_with_params();
+})
 
 btn_get_response.addEventListener("click", function() {
     //greet();    
-    //get_response_api();
-    get_data();
-    // logFetch();
+    get_response_api();
+    //get_data();
+    // get_data_arrow();
 });
 
 function hidden_section() {
@@ -23,53 +29,118 @@ function hidden_section() {
 function greet() {
     alert("Says: Hello World with alert")    
 };
-// function get_response_api(){
-//     //Step 1 
-//     let xhttp = new XMLHttpRequest();
+function get_response_api(){
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
 
-//     //Declare type of document
-//     xhttp.responseType = "json";
+        // Do the usual XHR stuff
+        let xhttp = new XMLHttpRequest();
+        //For get de document, selec url, and true for asynchronic method
+        xhttp.open("GET", URL, true);
+        xhttp.responseType = "json";
+        xhttp.onload = function(){
+            // This is called even on 404 etc
+            // so check the status
+            if((this.readyState === 4) && (this.status ===200)){
+                resolve(xhttp.response);
+                console.log(this.response.value.joke);
+                api_response.innerHTML = this.response.value.joke;
+                api_response.classList.remove("background-red");
+
+            }
+            else{
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                 reject(Error(xhttp.statusText));
+                 api_response.innerHTML = "Network Error";
+                 api_response.classList.add("background-red");
+            }
+        };
+        // Handle network errors
+        xhttp.onerror = function() {
+        reject(Error("Network Error"));
+        api_response.innerHTML = "Network Error";
+        api_response.classList.add("background-red");
+        };
+        
+        // Make the request
+        xhttp.send();
     
-//     //Step 2 Function for get de document, selec url, and true for asynchronic method
-//     xhttp.open("GET", URL, true);
-
-//     //Step 3 send the document
-//     xhttp.send();
-
-//     //Step 4 valid the document state 4(request finished and response is ready) and status 200(OK)
-//     xhttp.onreadystatechange = function(){
-//         if(this.readyState === 4){
-//             if(this.status === 200){
-//                 console.log(this.response.value.joke);
-//                     api_response.innerHTML = this.response.value.joke;
-//             }
-//             else{
-//                 console.log(this.statusText);
-//             }
-//         }
-//     } 
-// };
-
-// function get_data_arrow(){
-//     fetch(URL)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data.value.joke);
-//         api_response.innerHTML = data.value.joke;
-//     }).catch(err => {
-//        console.error('fetch failed', err);
-//     });
-
-// }
-function get_data(){
-    fetch(URL)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      console.log(response.value.joke);
-      api_response.innerHTML = response.value.joke;
-  }).catch(function(err){
-        console.error('fetch failed', err);
-      });
+    });
 }
+// function get_data_arrow(){
+//     return new Promise( (resolve, reject) => {
+
+//         fetch(URL)
+//         .then(res => {
+//             if(res.ok){
+//                return res.json();
+//             }
+//             reject("Error! Page not found. Status: " + res.status);      
+//         })
+//         .then(data => {
+//             resolve(data);
+//             console.log(data.value.joke);
+//             api_response.innerHTML = data.value.joke;
+//             api_response.classList.remove("background-red");
+//         }).catch(err => {
+//             reject(Error("Network Error"));
+//             console.error('fetch failed', err);
+//             api_response.innerHTML = err;
+//             api_response.classList.add("background-red");
+//         });
+//     });
+// }
+
+function get_data(){
+    return new Promise( (resolve, reject) => {
+        fetch(URL)
+        .then(function(response) {
+            if(response.ok){
+                return response.json();
+            }  
+            reject("Error! Page not found. Status: " + response.status);      
+        })
+        .then(function(response) {
+            resolve(response);
+            console.log(response.value.joke);
+            api_response.innerHTML = response.value.joke;
+            api_response.classList.remove("background-red");
+        }).catch(function(err){
+            reject(Error("Network Error"));
+            console.error('fetch failed', err);
+            api_response.innerHTML = err;
+            api_response.classList.add("background-red");
+        });
+    });
+};
+
+function get_data_with_params(){
+    fetch(URL2)
+    .then(function(res){
+      if(!res.ok){
+        console.log("error");
+      }
+      return res.json();
+    })
+    .then(function(res) {
+        let repo_searched = document.querySelector(".repository-name").value.toLowerCase();
+        console.log(repo_searched);
+      
+        let container_list = document.querySelector(".container-list");
+        container_list.innerHTML = "";
+        console.log(res.documentation_url);          
+        let repo_name = res;
+        if ( repo_searched === repo_name) {
+            container_list.innerHTML += "<li>" + data.resource + "</li>"  ;
+        }
+        else {
+            container_list.innerHTML += res.errors["0"].code;
+        }
+            
+    })
+    
+    .catch(function(e){
+      console.log(e);
+    });
+  }
